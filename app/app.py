4367c306028a606678ce7f8e2069535b157e8d9c
Emir Balkan <emir_balkan@hotmail.com>
2022-03-05 16:02:07 +0300

    Adding ORM version of managing database in app.py

--- app/app.py
+++ app/app.py
@@ -89,3 +89,65 @@ def delete_user_details(user_id):
         return json.dumps('Deleted'), HTTPStatus.OK
     except Exception as e:
         return json.dumps('Failed. ' + str(e)), HTTPStatus.NOT_FOUND        
+
+
+# ORM (bject Relational Model) version ->
+
+@app.route('/v2/users/<user_id>', methods=['GET'])
+def get_user_details_orm(user_id):
+    try:
+        result = db.session.query(Users).filter_by(id=user_id).first()
+        return json.dumps(
+            {
+                "name":result.name,
+                "surname":result.surname,
+                "identity_number":result.identity_number,
+                "links": hateoas(user_id)
+            }
+        ), HTTPStatus.OK
+    except Exception as e:
+        return json.dumps('Failed to retrieve record. ' + str(e)), HTTPStatus.NOT_FOUND
+
+
+@app.route('/v2/users', methods=['POST'])
+def post_user_details_orm():
+    try:
+        data = request.get_json()
+        user = Users(name=data['name'], surname=data['surname'], identity_number=data['identity_number'])
+        db.session.add(user)
+        db.session.commit()
+        return json.dumps(
+            {
+                "id": user.id,
+                "links": hateoas(user.id)
+            }
+        ), HTTPStatus.OK
+    except Exception as e:
+        return json.dumps('Failed to add record. ' + str(e)), HTTPStatus.NOT_FOUND
+
+
+@app.route('/v2/users/<user_id>', methods=['PATCH'])
+def patch_user_details_orm(user_id):
+    try:
+        data = request.get_json()
+        user = db.session.query(Users).filter_by(id=user_id).update(data)
+        db.session.commit()
+        return json.dumps(
+            {
+                "id": user_id,
+                "links": hateoas(user_id)
+            }
+    )
+    except Exception as e:
+        return json.dumps('Failed to update record. ' + str(e)), HTTPStatus.NOT_FOUND
+
+        
+@app.route('/v2/users /<user_id>', methods=['DELETE'])
+def delete_user_details_orm(user_id):
+    try:
+        user = db.session.query(Users).filter_by(id=user_id).first()
+        db.session.delete(user)
+        db.session.commit()
+        return json.dumps('Deleted'), HTTPStatus.OK
+    except Exception as e:
+        return json.dumps('Failed. ' + str(e)), HTTPStatus.NOT_FOUND        
